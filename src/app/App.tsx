c2734c10d373e6b3f8d83bdf18495987c734d179
unknown <steamistl@gmail.com>
2026-04-29 16:27:10 +0400

    добавил защищенный роут

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import { Home, Cabinet } from "@/pages/index";
+import { ProtectedRoute } from "./router/protected-route";
 import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>} />
-                <Route path="/cabinet" element={<Cabinet/>}></Route>
+                <Route path="/cabinet" element={
+                    <ProtectedRoute>
+                        <Cabinet/>
+                    </ProtectedRoute>}></Route>
             </Routes>
         </BrowserRouter>
     );
